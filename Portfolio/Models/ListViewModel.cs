14b309db76ba3dbcb0a58a1f9e79c37ae3567924
Anatolio <89778964651@mail.ru>
2021-05-10 02:38:12 +0300

    общая модель для двух типов данных

---
+++ Portfolio/Models/ListViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog;
+using Blog.Objects;
+
+namespace Portfolio.Models
+{
+    public class ListViewModel
+    {
+        public ListViewModel(IBlogRepository _blogRepository, int p)
+        {
+            Posts = _blogRepository.Posts(p - 1, 10);
+            TotalPosts = _blogRepository.TotalPosts();
+        }
+
+        public IList<Post> Posts { get; private set; }
+        public int TotalPosts { get; private set; }
+    }
+}
