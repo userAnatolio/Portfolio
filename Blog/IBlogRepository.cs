627b1b2357e442ec800985bd28ed9606c6c1e243
Anatolio <89778964651@mail.ru>
2021-05-10 02:36:26 +0300

    create IBlogRepository

---
+++ Blog/IBlogRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Blog.Objects;
+
+
+namespace Blog
+{
+    public interface IBlogRepository
+    {
+        //Метод Posts используется для последних опубликованных записей
+        IList<Post> Posts(int pageNo, int pageSize);
+        //TotalPosts используется для возврата общего кол-ва всех опубликованных сообщений
+        int TotalPosts();
+    }
+}
