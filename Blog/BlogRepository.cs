b59d328de0edd7ddad545927d50712a06999263e
Anatolio <89778964651@mail.ru>
2021-05-10 02:34:58 +0300

    create BlogRepository

---
+++ Blog/BlogRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+using Blog.Objects;
+using Blog.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private BlogContext _blogContext = new BlogContext();
+
+        public IList<Post> Posts(int pageNo, int pageSize)
+        {
+            //var obj = _blogContext.Posts.Find();
+            
+            List<Post> posts = new List<Post>();
+
+            return posts;
+        }
+
+        public int TotalPosts()
+        {
+            return 2;
+        }
+    }
+}
