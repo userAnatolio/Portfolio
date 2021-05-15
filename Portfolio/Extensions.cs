8e48bb34d2a4ac9d1b3d26a3e5c76c95cef9a48e
Anatolio <89778964651@mail.ru>
2021-05-15 23:28:05 +0300

    Дополнительные методы, пока не задействованны

--- Portfolio/Extensions.cs
+++ Portfolio/Extensions.cs
@@ -1,17 +1,12 @@
-﻿using Blog.Objects;
-using System;
-using System.Configuration;
-using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Blog
+namespace Portfolio
 {
     public static class Extensions
     {
 
-        public static string ToConfigLocalTime(this DateTime utcDT)
-        {
-            var istTZ = TimeZoneInfo.FindSystemTimeZoneById(ConfigurationManager.AppSettings["Timezone"]);
-            return String.Format("{0} ({1})", TimeZoneInfo.ConvertTimeFromUtc(utcDT, istTZ).ToShortDateString(), ConfigurationManager.AppSettings["TimezoneAbbr"]);
-        }
     }
 }
