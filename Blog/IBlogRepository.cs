b0e0e99293a92ddbae5bea8468deb3c002db71b3
Anatolio <89778964651@mail.ru>
2021-05-16 13:16:53 +0300

    методы для извлечения постов определенной категори и возврата общего кол-ва записей для данной категории

--- Blog/IBlogRepository.cs
+++ Blog/IBlogRepository.cs
@@ -12,5 +12,14 @@ namespace Blog
         IList<Post> Posts(int pageNo, int pageSize);
         //TotalPosts используется для возврата общего кол-ва всех опубликованных сообщений
         int TotalPosts(bool checkIsPublished = true);
+        //возвращает посты относящиеся к категории основанных на slug(UrlSlug)
+        IList<Post> PostsForCategory(string categorySlag, int pageNo, int pageSize);
+        //возвращает общее кол-во записей основанных на данной категории
+        int TotalPostsForCategory(string categorySlug);
+        //возвращает экземпляр категории
+        Category Category(string categorySlug);
     }
+
+   
+    
 }
