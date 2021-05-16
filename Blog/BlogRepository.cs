a111b80613c4ff3aefd58127e0ff6849d227ae37
Anatolio <89778964651@mail.ru>
2021-05-16 13:40:53 +0300

    Реализация методов интерфейса для извлечения записей на основе категории

--- Blog/BlogRepository.cs
+++ Blog/BlogRepository.cs
@@ -37,6 +37,37 @@ namespace Blog
             return _blogContext.Posts.Where(p => !checkIsPublished || p.Published == true).Count();
         }
 
+        //******************Реализация методов интерфейса для извлечения записей на основе категории**********************
+        public IList<Post> PostsForCategory(string categorySlug, int pageNo, int pageSize)
+        {
+            var posts = _session.Query<Post>()
+                                  .Where(p => p.Published && p.Category.UrlSlug.Equals(categorySlug))
+                                  .OrderByDescending(p => p.PostedOn)
+                                  .Skip(pageNo * pageSize)
+                                  .Take(pageSize)
+                                  .Fetch(p => p.Category)
+                                  .ToList();
 
+            var postIds = posts.Select(p => p.Id).ToList();
+
+            return _session.Query<Post>()
+                  .Where(p => postIds.Contains(p.Id))
+                  .OrderByDescending(p => p.PostedOn)
+                  .FetchMany(p => p.Tags)
+                  .ToList();
+        }
+
+        public int TotalPostsForCategory(string categorySlug)
+        {
+            return _session.Query<Post>()
+                          .Where(p => p.Published && p.Category.UrlSlug.Equals(categorySlug))
+                          .Count();
+        }
+
+        public Category Category(string categorySlug)
+        {
+            return _session.Query<Category>().FirstOrDefault(t => t.UrlSlug.Equals(categorySlug));
+        }
+        //******************************************************************************************************************
     }
 }
