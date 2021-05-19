9e9db9fc7eed67c8b872fa4ad1a640590c7b1cae
Anatolio <89778964651@mail.ru>
2021-05-19 22:28:03 +0300

    перегруженный конструктор с новым параметром название категории для свойства Category

--- Portfolio/Models/ListViewModel.cs
+++ Portfolio/Models/ListViewModel.cs
@@ -15,7 +15,15 @@ namespace Portfolio.Models
             TotalPosts = _blogRepository.TotalPosts();
         }
 
+        public ListViewModel(IBlogRepository blogRepository, string categorySlug, int p)
+        {
+            Posts = blogRepository.PostsForCategory(categorySlug, p - 1, 10);
+            TotalPosts = blogRepository.TotalPostsForCategory(categorySlug);
+            Category = blogRepository.Category(categorySlug);
+        }
+
         public IList<Post> Posts { get; private set; }
         public int TotalPosts { get; private set; }
+        public Category Category { get; private set; }
     }
 }
