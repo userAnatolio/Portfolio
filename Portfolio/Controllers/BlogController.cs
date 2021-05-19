bff8372cd3b15ea391474a02c4e87ed7992d390b
Anatolio <89778964651@mail.ru>
2021-05-19 22:24:43 +0300

    Новый метод действия принимает название категории и возвращает все посты для данной категории

--- Portfolio/Controllers/BlogController.cs
+++ Portfolio/Controllers/BlogController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Blog;
 using Blog.Objects;
 using Portfolio.Models;
+using System.Web;
+using System.Runtime.Serialization;
 
 namespace Portfolio.Controllers
 {
@@ -25,6 +27,50 @@ namespace Portfolio.Controllers
 
             ViewBag.Title = "Latest Posts";
             return View("List", viewModel);
+        }
+
+        public ViewResult Category(string category, int p)
+        {
+            var viewModel = new ListViewModel(_blogRepository, category, p);
+            if(viewModel.Category == null)
+            {
+                throw new HttpExteption(404, "Category Not Found");
+
+
+            }
+
+            ViewBag.Title = String.Format(@"Latest posts on category ""{0}""",
+                        viewModel.Category.Name);
+            return View("List", viewModel);
+        }
+    }
+
+    [Serializable]
+    internal class HttpExteption : Exception
+    {
+        private int v1;
+        private string v2;
+
+        public HttpExteption()
+        {
+        }
+
+        public HttpExteption(string message) : base(message)
+        {
+        }
+
+        public HttpExteption(int v1, string v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public HttpExteption(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected HttpExteption(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
         }
     }
 }
