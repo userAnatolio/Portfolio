7be152cb1b37c6341c6566a7a168ebef5f828f26
Anatolio <89778964651@mail.ru>
2021-07-10 20:34:26 +0300

    a new view model called WidgetViewModel

---
+++ Portfolio/Models/WidgetViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Portfolio;
+using Blog;
+using Blog.Objects;
+using System.Collections.Generic;
+
+namespace Models
+{
+    public class WidgetViewModel
+    {
+        public WidgetViewModel(IBlogRepository blogRepository)
+        {
+            Categories = blogRepository.Categories();
+        }
+
+        public IList<Category> Categories { get; private set; }
+    }
+}
